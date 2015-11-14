226aa5080a41acc0c2e46ec2555c33c8cf01bb72
KeisukeYoshida <y.keisuke0220@gmail.com>
2015-11-14 20:29:20 +0900

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirect_to user
+      redirect_back_or user #デフォかリクエストのページに移動する。nilならデフォに移動。
     else
       flash.now[:error] = 'Invalid email/password combination' # 誤りあり!
       render 'new'
@@ -16,6 +16,6 @@ class SessionsController < ApplicationController
 
   def destroy
     sign_out
-    redirect_to root_url
+    redirect_to root_url #最初に見れるサイトに移動する。sessionが切れる。
   end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,14 @@
 FactoryGirl.define do
   factory :user do
-    name     "Keisuke Yoshida"
-    email    "y.keisuke0220@gmail.com"
+    sequence(:name)   { |n| "Person #{n}" }
+    sequence(:email)  { |n| "person_#{n}@example.com"}
+    #name     "Keisuke Yoshida"
+    #email    "y.keisuke0220@gmail.com"
     password "foobar"
     password_confirmation "foobar"
+  
+    factory :admin do
+      admin true
+    end
   end
 end
